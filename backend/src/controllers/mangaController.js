5cb7d41efca35da7131ad78406243b21dbdb2bfe
quypq147 <phanngocquy1472004@gmail.com>
2026-01-15 10:48:21 +0700

    [dev] set up MongoDB

---
+++ backend/src/controllers/mangaController.js
@@ -0,0 +1,7 @@
+import {Request, Response} from 'express';
+
+export function getMangaList(req: Request, res: Response) {
+    try{
+    
+    }
+}
---
+++ backend/src/services/manga.js
@@ -0,0 +1,9 @@
+function fetchMangaListFromDB() {
+    try{
+        
+    }
+    catch (error) {
+        console.error("Error fetching manga list:", error);
+        throw error;
+    }
+}
