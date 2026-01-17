36d035d721794625614c4e441f9901f669d029c0
quypq147 <phanngocquy1472004@gmail.com>
2026-01-17 09:37:09 +0700

    [dev] update

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,11 +3,16 @@ const mongoose = require('mongoose');
 const UserSchema = mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    fullname: { type: String, required: true },
     passwordHash: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
-    roles: [String],
-    isActive: { type: Boolean, default: true }
+    role: { 
+        type: String, 
+        enum: ['user', 'admin', 'uploader', 'moderator'], 
+        default: 'user' 
+    },
+    isDisabled: { type: Boolean, default: false },
+    isActive: { type: Boolean, default: true },
+    isLinkedToExternalService: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('User', UserSchema);
